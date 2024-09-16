5562c5ef793c47e453ecb48354083edf91edfbdf
Peter Jannesen <peter@jannesen.com>
2024-09-16 20:45:03 +0200

    Remove [Serializable] from Exceptions

--- Exceptions.cs
+++ Exceptions.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Runtime.Serialization;
 using System.Security.Permissions;
 
 namespace Jannesen.PushNotification
 {
-    [Serializable]
     public class PushNotificationConfigException: Exception
     {
         public                              PushNotificationConfigException(string message): base(message)
@@ -13,13 +12,8 @@ namespace Jannesen.PushNotification
         public                              PushNotificationConfigException(string message, Exception innerException): base(message, innerException)
         {
         }
-
-        protected                           PushNotificationConfigException(SerializationInfo info, StreamingContext context): base(info, context)
-        {
-        }
     }
 
-    [Serializable]
     public class PushNotificationConnectionException: Exception
     {
         public                              PushNotificationConnectionException(string message): base(message)
@@ -28,16 +22,11 @@ namespace Jannesen.PushNotification
         public                              PushNotificationConnectionException(string message, Exception innerException): base(message, innerException)
         {
         }
-
-        protected                           PushNotificationConnectionException(SerializationInfo info, StreamingContext context): base(info, context)
-        {
-        }
     }
 
-    [Serializable]
     public class PushNotificationException: Exception
     {
-        public          PushMessage        Notification             { get; private set; }
+        public          PushMessage         Notification             { get; private set; }
 
         public                              PushNotificationException(PushMessage notification, string message): base(message)
         {
@@ -47,54 +36,28 @@ namespace Jannesen.PushNotification
         {
             this.Notification = notification;
         }
-
-        protected                           PushNotificationException(SerializationInfo info, StreamingContext context): base(info, context)
-        {
-            Notification = (PushMessage)info.GetValue(nameof(Notification), typeof(PushMessage));
-        }
-        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
-        public override void                GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-            info.AddValue(nameof(Notification), Notification);
-        }
     }
 
-    [Serializable]
     public class PushNotificationInvalidDeviceException: PushNotificationException
     {
         public                              PushNotificationInvalidDeviceException(PushMessage notification): base(notification, "Invalid device-token '" + notification.DeviceToken + "'.")
         {
         }
-
-        protected                           PushNotificationInvalidDeviceException(SerializationInfo info, StreamingContext context): base(info, context)
-        {
-        }
     }
 
-    [Serializable]
     public class PushNotificationExpiredException: PushNotificationException
     {
         public                              PushNotificationExpiredException(PushMessage notification): base(notification, "Notification expired device-token '" + notification.DeviceToken + "'.")
         {
         }
-
-        protected                           PushNotificationExpiredException(SerializationInfo info, StreamingContext context): base(info, context)
-        {
-        }
     }
 
-    [Serializable]
     public class PushNotificationServiceException: Exception
     {
         public                              PushNotificationServiceException(string message): base(message)
         {
         }
         public                              PushNotificationServiceException(string message, Exception innerException): base(message, innerException)
-        {
-        }
-
-        protected                           PushNotificationServiceException(SerializationInfo info, StreamingContext context): base(info, context)
         {
         }
     }
